1b63bd6353a7fe8ff71386c55c80c3d0a1a0e98f
Russell Blickhan <rwblickhan@gmail.com>
2023-12-19 23:32:51 -0800

    Clean up some styling

--- server.ts
+++ server.ts
@@ -1,16 +1,8 @@
-import {
-  serveDir,
-  serveFile,
-} from "https://deno.land/std@0.207.0/http/file_server.ts";
+import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
 
 Deno.serve((req: Request) => {
-  const pathname = new URL(req.url).pathname;
-
-  if (pathname.startsWith("/pagefind")) {
-    return serveDir(req, {
-      fsRoot: "public/pagefind",
-      urlRoot: "pagefind",
-    });
-  }
-  return serveFile(req, "public/index.html");
+  return serveDir(req, {
+    fsRoot: "public",
+    urlRoot: "",
+  });
 });
