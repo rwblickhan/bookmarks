10aa27abe21d14c3b261b2a78bd03320b359b81c
Russell Blickhan <rwblickhan@gmail.com>
2024-03-31 20:35:57 -0700

    Update justfile rules

--- justfile
+++ justfile
@@ -2,6 +2,3 @@ alias up := update
 
 update:
     deno task export_goodlinks && deno task export_obsidian && deno task fetch && deno task raindrop_export
-
-serve:
-    deno task serve
--- server.ts
+++
@@ -1,8 +0,0 @@
-import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
-
-Deno.serve((req: Request) => {
-  return serveDir(req, {
-    fsRoot: "public",
-    urlRoot: "",
-  });
-});
